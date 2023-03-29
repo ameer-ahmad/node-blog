179fe466f82de86b284504f1b039c173a5af1409
ameer-ahmad <ahmaamee@sheridancollege.ca>
2023-03-29 12:55:38 -0400

    post request for creating new blog

--- app.js
+++ app.js
@@ -1,32 +1,69 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 // express app
 const app = express()
 
+// connect to mongoDB
+const dbURI = 'mongodb+srv://ameer:zMYakD157Rz6QxBI@nodeblog.jp3yefq.mongodb.net/NodeBlog?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000))
+    .catch((error) => console.error(error))
+
 // register view engine
 app.set('view engine', 'ejs')
 
-// listen for requests
-app.listen(3000)
 
 // middleware & static files
 app.use(express.static('public'))
-
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ]
-    res.render('index', { title: 'Home', blogs})
 
+app.get('/', (req, res) => {
+    res.redirect('/blogs')
 })
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About'})
+})
+
+
+// blog routes
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', {title: 'All Blogs', blogs: result})
+        })
+        .catch(err => {
+            console.error(err)
+        })
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+
+    blog.save()
+        .then(result => {
+            res.redirect('/blogs')
+        })
+        .catch(err => {
+            console.error(err)
+        })
+})
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id
+    Blog.findById(id)
+        .then(result => {
+            res.render('details', { blog: result, title: 'Blog Details'})
+        })
+        .catch(err => {
+            console.error(err)
+        })
 })
 
 app.get('/blogs/create', (req, res) => {
