a7b9fa9f5fb3cddd62fa2bcaca192c9887f02d00
ameer-ahmad <ahmaamee@sheridancollege.ca>
2023-03-29 13:32:38 -0400

    separated logic into controller

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,55 +1,15 @@
 const express = require('express')
-const Blog = require('../models/blog')
+const blogController = require('../controllers/blogController')
 const router = express.Router()
 
-router.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-  });
+router.get('/blogs/create', blogController.blog_create_get);
   
-  router.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-      .then(result => {
-        res.render('index', { blogs: result, title: 'All blogs' });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
+  router.get('/blogs', blogController.blog_index);
   
-  router.post('/blogs', (req, res) => {
-    // console.log(req.body);
-    const blog = new Blog(req.body);
+  router.post('/blogs', blogController.blog_create_post);
   
-    blog.save()
-      .then(result => {
-        res.redirect('/blogs');
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
+  router.get('/blogs/:id', blogController.blog_details);
   
-  router.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-      .then(result => {
-        res.render('details', { blog: result, title: 'Blog Details' });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-  
-  router.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    
-    Blog.findByIdAndDelete(id)
-      .then(result => {
-        res.json({ redirect: '/blogs' });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
+  router.delete('/blogs/:id', blogController.blog_delete);
 
   module.exports = router
